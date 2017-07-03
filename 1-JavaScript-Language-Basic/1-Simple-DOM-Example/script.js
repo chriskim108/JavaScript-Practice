172b9a2274b28b3964cc6af481e8238b3d0db9f8
Christopher Kim <chriskim108@gmail.com>
2017-07-03 06:02:01 -0400

    Add files via upload

---
+++ 1-JavaScript-Language-Basic/1-Simple-DOM-Example/script.js
@@ -0,0 +1,27 @@
+//How to see an output in JavaScript?
+//window.alert();
+	//This is throwing an alert message onto the browser
+	//Alternative 
+		//alert();
+//document.write()
+	//Choosing the specific document you would maniuplate it
+//innerHTML
+	//This will manipulate the HTML tags
+//console.log()
+	//Print the result on the inspector
+function alertButton(){
+	window.alert(5+6);
+}
+    
+function Sum(){
+	document.write(5+11);
+}
+
+function myfunction(){
+	document.getElementById('p-demo').innerHTML = 'This is the new P-tag';
+}
+
+function consoleButton(){
+	console.log("Hello World");
+}
+
---
+++ 1-JavaScript-Language-Basic/2-Event-Example/script.js
@@ -0,0 +1,17 @@
+
+
+// List of a few events on JavaScript
+	// onclick
+	// onchange
+	// onmouseover
+	// onmouseout
+	// onkeydown
+	// onload
+
+//onclick Examples
+//Example 1 
+function paragraph_changer(){
+	document.getElementById('demo').innerHTML="The p-tag has been changed"
+}
+
+
